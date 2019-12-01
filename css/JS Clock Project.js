18629ba0b1c004e8913b91eee728ffbb5f5e1636
Dalvin Cash <cashdalvin1@gmail.com>
2019-11-30 20:43:14 -0600

    JS Extra Practice (Clock Project)

---
+++ css/JS Clock Project.js
@@ -0,0 +1,18 @@
+var showCurrentTime = function(){
+    var clock = document.getElementByID("clock");
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var meridian = "AM";
+    if (hours >= noon){
+        meridian = "PM";
+    }
+   if (hours > noon){
+    hours = hours - 12;
+   }
+   if(minutes < 10){
+       minutes = "0" + minutes;
+   }
+   if (seconds = "0" + seconds);
+}
