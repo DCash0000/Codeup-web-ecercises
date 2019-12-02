17c1116cf9c9306d1124045bda2d689149ef7ea2
Dalvin Cash <cashdalvin1@gmail.com>
2019-12-01 22:06:55 -0600

    Java.js practice

--- Java.js
+++ Java.js
@@ -1,3 +1,12 @@
 console.log("Hello World");
-let name = "Mosh";
+
+let name = "Cash";
 console.log(name);
+
+//variable names should be meaningful and descriptive
+//variable names cannot start with a number
+//variable names cannot contain spaces or hyphens (-)
+//variable names are case sensitive
+
+const interestRate = 0.3;
+console.log(interestRate);
