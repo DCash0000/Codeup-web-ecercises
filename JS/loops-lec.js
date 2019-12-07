402df7fa908fa521e77b9ae712e0100eef5fb920
Dalvin Cash <cashdalvin1@gmail.com>
2019-12-06 19:02:23 -0600

    loops

--- JS/loops-lec.js
+++ JS/loops-lec.js
@@ -207,12 +207,12 @@
 
 // We can get an iteration to skip using continue;
 
-for (var i = 1; i < 100; i++) {
-
-	if (i % 2 !== 0) {
-		// skipping all odd numbers;
-		continue;
-	}
-
-	console.log('Here is a lovely even number: ' + i);
-}
+// for (var i = 1; i < 100; i++) {
+//
+// 	if (i % 2 !== 0) {
+// 		// skipping all odd numbers;
+// 		continue;
+// 	}
+//
+// 	console.log('Here is a lovely even number: ' + i);
+// }
