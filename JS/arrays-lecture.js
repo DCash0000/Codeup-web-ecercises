00219c9fb670443b61cf5d8a137594e24514ae95
Dalvin Cash <cashdalvin1@gmail.com>
2019-12-10 12:18:00 -0600

    javascript manipulating

--- JS/arrays-lecture.js
+++ JS/arrays-lecture.js
@@ -12,22 +12,100 @@ let pets = [];
  pets[5] = "Malu";
 
 
+//length is used to find out the size of that object
+for(var i = 0; i < pets.length; i++){
+    // console.log(pets[i]);
+}
 
-// for(var i = 0; i < pets.length; i++){
-//     console.log(pets[i]);
-// }
-//
-// var i = 0;
-// while (i < pets.length){
-//     console.log(pets[i]);;
-//     i++
-// }
+var i = 0;
+while (i < pets.length){
+    // console.log(pets[i]);;
+    i++
+}
 
 let laptops = ["mac", "toshiba", "dell", "lenovo"];
-for(let i = 0; i < laptops.length; i++){
-    console.log(laptops[i]);
+for(let i = 0; i < laptops.length ; i++){
+    // console.log(laptops[i]);
 
 }
 
+//push method adds items to the end of the array
+var fruit = ["apple", "orange", "banana"];
+// console.log(fruit);
+fruit.push("pear");
+// console.log(fruit);
+
+
+//unshift adds items to the beginning of an array
+fruit.unshift("kiwi");
+// console.log(fruit);
+
+
+
+//pop and shift used to remove items from an array
+
+
+
+let drinks = ["soda", "tea", "water", "energy drink", "lemonade","tea", "kombucha"];
+console.log(drinks);
+// drinks.push("lemonade");
+// console.log(drinks);
+// drinks.unshift("powerade");
+// console.log(drinks);
+//
+// let endRemoveItem = drinks.pop();
+// console.log(endRemoveItem);
+//
+// let begRemoveItem = drinks.shift();
+// console.log(begRemoveItem);
+
+// let index = drinks.indexOf("tea");
+// console.log(index);
+//
+// let indexA = drinks.lastIndexOf(("tea"));
+// console.log(indexA);
+
+// let slice = drinks.slice(1, 6);
+// console.log(slice);
+//
+// slice = drinks.slice(3);
+// console.log(slice);
+
+// drinks.reverse();
+// console.log(drinks);
+
+// drinks.sort();
+// console.log(drinks);
+
+// let drinkArray = drinks.join(", ");
+// console.log(drinkArray);
+
+// let drank = "Good Drank";
+// console.log(drank);
+//
+// let drankString = drank.split(", ");
+// console.log(drankString);
+
+
+
+//Take the first letter of each word and make it Upper Case
+// var sentence = "The quick brown fox jumps over the lazy dog";
+// var words = sentence.split(" ");
+// for (var i = 0;i<words.length; i++) {
+//  // capitalize first letter
+//  var word = words[i];
+//  // var firstLetter = word.split()[0];
+//  var letters = word.split("");
+//  var capitalFirstLetter = letters[0].toUpperCase();
+//  var capitalizedWord = "";
+//  capitalizedWord += capitalFirstLetter;
+//  for (var j=1;j<letters.length;j++) {
+//   capitalizedWord += letters[j];
+//  }
+//  words[i] = capitalizedWord;
+// }
+// words.reverse();
+// sentence = words.join(" ");
+// console.log(sentence);
 
 
--- JS/planets-array.js
+++ JS/planets-array.js
@@ -12,29 +12,58 @@
         'Neptune'
     ];
 
+ let planetsUl = "<ul><li>" + planets.join("</li><li>") + "</ul></li>";
+    console.log(planetsUl);
+
     /**
      * TODO:
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
 
-    console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets);
+    // console.log('Adding "The Sun" to the beginning of the planets array.');
+    // console.log(planets);
 
-    console.log('Adding "Pluto" to the end of the planets array.');
-    console.log(planets);
+    // planets.unshift("Sun");
+    // console.log(planets);
 
-    console.log('Removing "The Sun" from the beginning of the planets array.');
-    console.log(planets);
+    // console.log('Adding "Pluto" to the end of the planets array.');
+    // console.log(planets);
 
-    console.log('Removing "Pluto" from the end of the planets array.');
-    console.log(planets);
+    // planets.push("Pluto");
+    // console.log(planets);
 
-    console.log('Finding and logging the index of "Earth" in the planets array.');
+    // console.log('Removing "The Sun" from the beginning of the planets array.');
+    // console.log(planets);
 
-    console.log("Reversing the order of the planets array.");
-    console.log(planets);
+    // let firstItem = planets.shift();
+    // console.log(firstItem);
 
-    console.log("Sorting the planets array.");
-    console.log(planets);
+
+    // console.log('Removing "Pluto" from the end of the planets array.');
+    // console.log(planets);
+
+    // let lastItem = planets.pop();
+    // console.log(lastItem);
+
+
+    // console.log('Finding and logging the index of "Earth" in the planets array.');
+
+    // let index = planets.indexOf("Earth");
+    // console.log(index);
+
+    // console.log("Reversing the order of the planets array.");
+    // console.log(planets);
+
+    // planets.reverse();
+    // console.log(planets);
+
+    // console.log("Sorting the planets array.");
+    // console.log(planets);
+
+    // planets.sort();
+    // console.log(planets);
+
+
+
 })();
