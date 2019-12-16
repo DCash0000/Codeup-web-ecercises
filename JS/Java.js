ff4416aebd3c629d930ff3a55ce56898aa6a5666
Dalvin Cash <cashdalvin1@gmail.com>
2019-12-16 11:25:54 -0600

    JS DOM exercise

--- JS/Java.js
+++ JS/Java.js
@@ -206,3 +206,10 @@ for(let i = 0; i < bobsFollowers.length; i++){
         }
     }
 }
+//The While Loop
+const cards = ["diamond", "spade", "heart", "club"];
+let currentCard = 5;
+while(currentCard !== "spade"){
+    currentCard = cards[Math.floor(Math.random() * 4)];
+    console.log(currentCard);
+}
