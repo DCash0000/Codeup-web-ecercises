5dfaa5625b516b36cb5e478db54f9dc0db2d17a0
Dalvin Cash <cashdalvin1@gmail.com>
2020-01-21 15:00:36 -0600

    Promises exercise completed

--- JS/keys.js
+++ JS/keys.js
@@ -1,1 +1,2 @@
 let mapboxToken = "pk.eyJ1IjoiZGNhc2giLCJhIjoiY2s0YTA1aXpqMGFyeDNtbXM5NmcxdHVodyJ9.zwZ6k20SBaKIWdNFae4GEg";
+let gitHub = 'token 3d0abb13e1a3cfd7c6c3eac3ce4da8692c1e56bf';
---
+++ JS/promises.js
@@ -0,0 +1,54 @@
+//**************Step 2**************//
+const wait = (num) => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve();
+        }, num)
+
+    })
+};
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+//******************Step 3**************//
+// function getGithubUsernames(name) {
+//     return fetch(`https://api.github.com/search/commits?q=sort:committer-date+committer:${name}`,
+//         {headers: {
+//                 'Authorization': gitHub,
+//                 'Accept': 'application/vnd.github.cloak-preview'
+//             }})
+//         .then(response => response.json())
+//         .then(response => {
+//             let commit = response.items[0].commit;
+//             let name = commit.author.name;
+//             let date = commit.author.date;
+//             console.log(`Last commit was on ${date} by ${name}`)
+//         });
+// }
+
+
+const lastCommit = (name) => {
+    return fetch(`https://api.github.com/search/commits?q=sort:committer-date+committer:${name}`,
+        {headers: {
+                'Authorization': gitHub,
+                'Accept': 'application/vnd.github.cloak-preview'
+            }})
+        .then(response => response.json())
+        .then(response => {
+            let commit = response.items[0].commit;
+            let name = commit.author.name;
+            let date = commit.author.date;
+            console.log(`Last commit was on ${date} by ${name}`)
+        });
+
+};
+
+console.log(lastCommit('DCash0000'));
+
+
+
+
+
+
