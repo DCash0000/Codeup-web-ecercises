eebca421739e962d2d7ed879867bf3c550141e2c
Dalvin Cash <cashdalvin1@gmail.com>
2020-01-23 09:49:54 -0600

    Update promises.js

--- JS/promises.js
+++ JS/promises.js
@@ -13,23 +13,7 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 //******************Step 3**************//
-// function getGithubUsernames(name) {
-//     return fetch(`https://api.github.com/search/commits?q=sort:committer-date+committer:${name}`,
-//         {headers: {
-//                 'Authorization': gitHub,
-//                 'Accept': 'application/vnd.github.cloak-preview'
-//             }})
-//         .then(response => response.json())
-//         .then(response => {
-//             let commit = response.items[0].commit;
-//             let name = commit.author.name;
-//             let date = commit.author.date;
-//             console.log(`Last commit was on ${date} by ${name}`)
-//         });
-// }
-
-
-const lastCommit = (name) => {
+function getGithubUsernames(name) {
     return fetch(`https://api.github.com/search/commits?q=sort:committer-date+committer:${name}`,
         {headers: {
                 'Authorization': gitHub,
@@ -42,10 +26,26 @@ const lastCommit = (name) => {
             let date = commit.author.date;
             console.log(`Last commit was on ${date} by ${name}`)
         });
+}
 
-};
 
-console.log(lastCommit('DCash0000'));
+// const lastCommit = (name) => {
+//     return fetch(`https://api.github.com/search/commits?q=sort:committer-date+committer:${name}`,
+//         {headers: {
+//                 'Authorization': gitHub,
+//                 'Accept': 'application/vnd.github.cloak-preview'
+//             }})
+//         .then(response => response.json())
+//         .then(response => {
+//             let commit = response.items[0].commit;
+//             let name = commit.author.name;
+//             let date = commit.author.date;
+//             console.log(`Last commit was on ${date} by ${name}`)
+//         });
+//
+// };
+//
+// console.log(lastCommit('DCash0000'));
 
 
 
